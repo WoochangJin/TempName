758dd3baa317dcb6c2c33f930d4d59c93f67cbd6
wchng <jwcgomunamu@gmail.com>
2025-09-14 20:15:29 +0900

    added mode turning function

--- Assets/scripts/MainMenuCarousel.cs
+++ Assets/scripts/MainMenuCarousel.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEngine.UI;   // UnityEngine.UI 꼭 필요!
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainMenuCarousel : MonoBehaviour
@@ -7,8 +8,9 @@ public class MainMenuCarousel : MonoBehaviour
     public TextMeshProUGUI modeText;
     public Button leftButton;
     public Button rightButton;
+    public Button goButton;
 
-    string[] modes = { "Tarot", "Cookie", "Zodiac" };
+    string[] modes = { "Mode1", "Mode2", "Mode3", "Mode4", "Mode5" };
     int currentIndex = 0;
 
     void Start()
@@ -16,6 +18,7 @@ public class MainMenuCarousel : MonoBehaviour
         modeText.text = modes[currentIndex];
         leftButton.onClick.AddListener(OnLeft);
         rightButton.onClick.AddListener(OnRight);
+        goButton.onClick.AddListener(turnMode);
     }
     void OnLeft()
     {
@@ -30,4 +33,17 @@ public class MainMenuCarousel : MonoBehaviour
         currentIndex = (currentIndex + 1) % modes.Length;
         modeText.text = modes[currentIndex];
     }
+
+    void turnMode()
+    {
+        string target = modes[currentIndex];
+        Debug.Log($"[MainMenu] Try load: '{target}'");
+        if (!Application.CanStreamedLevelBeLoaded(target))
+        {
+            Debug.LogError($"[MainMenu] Scene '{target}' 로드 불가! Build Settings에 추가됐는지/이름이 정확한지 확인.");
+            return;
+        }
+        SceneManager.LoadScene(target, LoadSceneMode.Single);
+    }
+
 }
